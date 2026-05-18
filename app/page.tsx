de06a35a4fe7371e879e6722cb2836084c49f215
Lyle Menard <lylemenard833@gmail.com>
2026-05-18 18:40:23 -0500

    initial commit, working boilerplate accepting click input on grid of buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,70 @@
+'use client';
 import Image from "next/image";
+import { useState } from 'react';
 
-export default function Home() {
+enum CellState { Empty, Solid, Marked };
+
+interface CellProps {
+  state: CellState;
+  onSquareClick: () => void;
+}
+
+function Cell({ state, onSquareClick }: CellProps) {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <button className="cell" onClick={onSquareClick}>
+      {state}
+    </button>
+  )
+}
+
+interface PuzzleGridProps {
+  rowDefinitions: number[][];
+  columnDefinitions: number[][];
+  puzzleState: CellState[][];
+  onMove: (newState: CellState[][]) => void;
+}
+
+function PuzzleGrid({ rowDefinitions, columnDefinitions, puzzleState, onMove }: PuzzleGridProps) {
+  function handleClick(row: number, column: number) {
+    console.log("handleClick at ", row, column)
+    let newPuzzleState = [...puzzleState];
+    newPuzzleState[row][column] = CellState.Solid;
+    onMove(newPuzzleState);
+  }
+  return <div>
+    {columnDefinitions.map((columnDefinition, columnIndex) =>
+      <div className="puzzle-grid-column" key={columnIndex}>
+        {rowDefinitions.map((rowDefinition, rowIndex) =>
+          <Cell state={puzzleState[rowIndex][columnIndex]} onSquareClick={() => handleClick(rowIndex, columnIndex)} key={rowIndex} />
+        )}
+      </div>
+    )}
+  </div>
+}
+
+
+
+export default function Puzzle() {
+  function handleMove(newState: CellState[][]) {
+    console.log("setting new state");
+    setPuzzleState(newState);
+  }
+
+  const puzzleWidth = 15;
+  const puzzleHeight = 15;
+  const puzzleSolution = [
+    [[15], [4, 5], [2, 4], [1, 3], [2], [2], [2, 4, 3], [2, 6, 2], [2, 1, 6, 2], [2, 1, 1, 4, 2], [1, 1], [1, 3, 2, 1], [2, 2, 1, 2, 1], [3, 3, 2, 1], [9]],
+    [[4, 4], [3, 1, 2, 3], [2, 1, 2, 2], [2, 1, 1], [1, 4, 2], [1, 3], [1, 8], [1, 3, 1, 1], [1, 4, 2, 1], [1, 4], [2, 4, 3], [3, 3, 3], [4, 1], [10, 3], [10]]
+  ];
+  const [puzzleState, setPuzzleState] = useState<CellState[][]>(
+    Array(puzzleWidth).fill(null).map(() => Array(puzzleHeight).fill(CellState.Empty))
+  );
+
+  return (
+    <div className="puzzle">
+      <div className="puzzle-grid">
+        <PuzzleGrid rowDefinitions={puzzleSolution[0]} columnDefinitions={puzzleSolution[1]} puzzleState={puzzleState} onMove={handleMove} />
+      </div>
     </div>
   );
 }
